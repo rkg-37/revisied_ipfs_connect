7abab53742cfce6c2a30f66fc9f67451102d3998
rkg-37 <gupta2.rajkamal2@gmail.com>
2022-12-04 11:51:32 +0530

    ticket expansion

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -25,36 +25,15 @@ const raiseTicket = async (req, res) => {
                 req.body.productOwner,
                 req.body.secret,
                 ticket_hash.toString(),
-		    process.env.TICKET_ADDRESS
+		        process.env.TICKET_ADDRESS
             );
-            const demo = await raiseTicketResponse.wait();
-
-            console.log(demo.events);
-        // decode ipfs details
-        // let responseObject = {
-        //     code: 200,
-        //     result: {
-        //         warrantyDetails: (await ipfsGetData(warranty[0])),
-        //         customerDetails: (await ipfsGetData(warranty[1])),
-        //         expiry_duration: warranty[2].toString(),
-        //         startTime: warranty[3].toString(),
-        //         endTime: warranty[4].toString(),
-        //         tracking: warranty[5].map(async (_t) => {
-        //             return await ipfsGetData(_t)
-        //         }),
-        //         acitvity: warranty[6].map(async (_a) => {
-        //             return await ipfsGetData(_a)
-        //         }),
-        //         status: warranty[7],
-        //         creationDate: (new Date(Number(warranty[8].toString()) * 1000))
-        //     },
-        //     message: "Warranty fetched!!!"
-        // }
-
-
-
-
-        return res.status(200).json({"data":raiseTicketResponse});
+            const log = await raiseTicketResponse.wait()
+            const transferEvent = log.events.filter(_e => _e.event == "Transfer")[0];
+            return res.status(200).json({
+                    ticketId: transferEvent.args[2].toString(),
+                    message: "Ticket raised"
+            })
+        
     } catch (err) {
         console.log(err);
         return res.status(500).json({
